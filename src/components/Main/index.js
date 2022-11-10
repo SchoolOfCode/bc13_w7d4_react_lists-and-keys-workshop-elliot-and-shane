fc4499883cf5d8982d650f3ebaa5febb12b5eb6f
elliot tasker <elliottasker@gmail.com>
2022-11-10 16:19:58 +0000

    task 4 completed

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './index.css';
+import "./index.css";
 
-import samplePosts from '../../libs/data';
+import samplePosts from "../../libs/data";
 
-import Post from '../Post';
+import Post from "../Post";
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+  return (
+    <main id="main">
+      {samplePosts.map(
+        ({ postId, title, author, text, highlights, image, link, alt, date }) => {
+          return (
+            <Post
+              title={title}
+              date={date}
+              author={author}
+              text={text}
+              highlights={highlights}
+              image={image}
+              link={link}
+              alt={alt}
+              key={postId}
+            />
+          );
+        }
+      )}
+    </main>
+  );
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
-function Post() {
-  return <article></article>;
+function Post({ title, author, text, highlights, image, link, alt, date }) {
+  return (
+    <article>
+      <h2> {title}</h2>
+      <p> {date} </p>
+      <p> {author}</p>
+      <p> {text}</p>
+    </article>
+  );
 }
 
 export default Post;
